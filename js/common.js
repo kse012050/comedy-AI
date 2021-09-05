03c8ab0d2ea54133ed8271edb421d4aed66e9d13
김성은 <kse012050@naver.com>
2021-09-06 08:02:47 +0900

    press page fin

--- js/common.js
+++ js/common.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $('.btn').append('<span class="material-icons-outlined">chevron_right</span>')
+    $('.btn01').append('<span class="material-icons-outlined">chevron_right</span>')
     
     $('.menuBtn').click(function(){
         $(this).toggleClass('active');
