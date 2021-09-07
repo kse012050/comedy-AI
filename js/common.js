c0b896b211a3aaff9a8cbc8269d237b5af4904d4
김성은 <kse012050@naver.com>
2021-09-07 16:44:31 +0900

    whoIs page animation

--- js/common.js
+++ js/common.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('.btn').append('<span class="material-icons-outlined">chevron_right</span>')
     $('.btn01').append('<span class="material-icons-outlined">chevron_right</span>')
     
+    
     $('.menuBtn').click(function(){
         $(this).toggleClass('active');
         $('nav').stop().fadeToggle();
