27a86afc8db8f79d993cc719da76ed083afa15da
김성은 <kse012050@naver.com>
2021-09-08 04:46:53 +0900

    fullpage event fin

--- js/common.js
+++ js/common.js
@@ -9,9 +9,32 @@ $(document).ready(function(){
     })
 
     var scrollSelecter;
+    var scrollBoolean = true;
+    var scrollIdx = 0;
+    var scrollOffset = 0;
 
-    $('body').on('mousewheel',function(){
-        scrollSelecter = $('main , footer');
-        console.log(scrollSelecter);
+    $('body.scrollArea').on('mousewheel',function(e){
+        if(scrollBoolean){
+            scrollBoolean = false;
+            setTimeout(() => {
+                scrollBoolean = true;
+            }, 500);
+            var delta = e.originalEvent.wheelDelta;
+            scrollSelecter = $('header + * , footer');
+            if(delta > 0 && scrollIdx > 0){
+                scrollIdx--;
+                scrollOffset = scrollSelecter.eq(scrollIdx).offset().top;
+            }else if(delta < 0 && scrollIdx < scrollSelecter.length - 1){
+                scrollIdx++;
+                scrollOffset = scrollSelecter.eq(scrollIdx).innerHeight();
+            }else{
+                return;
+            }
+            $('html').stop().animate({scrollTop : scrollOffset},500)
+        }
     })
 });
+
+window.onbeforeunload = function() {
+    window.scrollTo(0, 0);
+};
